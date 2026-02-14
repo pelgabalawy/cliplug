a82dfdb62b9e7b995ff128ea04dbdbc914d84d60
pelga <pelgabalawy@gmail.com>
2026-02-14 17:26:22 -0500

    Changed the name of the list command object

--- cliplug/cli.py
+++ cliplug/cli.py
@@ -1,5 +1,5 @@
 import click
-from cliplug.commands.list import list_commands
+from cliplug.commands.list import list_cmd
 from cliplug.commands.remove import delete
 from cliplug.commands.run import run
 from cliplug.commands.add import add
@@ -9,7 +9,7 @@ def main():
     """Cliplug - A CLI plugin system"""
     pass
 
-main.add_command(list_commands, name="list")
+main.add_command(list_cmd, name="list")
 main.add_command(run, name="run")
 main.add_command(add, name="add")
 main.add_command(delete, name="delete")
