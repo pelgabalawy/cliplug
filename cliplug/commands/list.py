ba259bd6d54c0f7d643928a7589cbbc4f172bdfb
pelga <pelgabalawy@gmail.com>
2026-02-14 17:26:44 -0500

    added keywords filter to the list command to make it more robust

--- cliplug/commands/list.py
+++ cliplug/commands/list.py
@@ -2,9 +2,24 @@ import click
 from cliplug.storage.yaml_store import load_data
 
 @click.command()
-def list_commands():
+@click.option(
+    '--keywords',
+    "-k",
+    required=False,
+    multiple=True,
+    default=None,
+    help="[Optional] filter command by keywords (-k foo -k bar for muli keywords)"
+)
+def list_cmd(keywords):
     data = load_data()
-    click.secho("----------------\nlist of commands:\n----------------")
-    for ref, com in data["commands"].items():
-        click.secho(f"\t{ref}: {com}", fg="green")
+    if data["commands"]:
+        click.secho("----------------\nlist of commands:\n----------------")
+        for ref, com in data["commands"].items():
+            if keywords:
+                if any([keyword in ref or keyword in com for keyword in keywords]):
+                    click.secho(f"\t{ref}: {com}", fg="green")
+            else:
+                click.secho(f"\t{ref}: {com}", fg="green")
+    else:
+        click.secho("No commands found!", fg="red")
 
